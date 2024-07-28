39de842423671cd78b1758e319a4fb192b4b70ec
ThomasCherian03 <gotothomas.1523@gmail.com>
2024-07-28 17:46:05 +0530

    Backend Api made

--- Backend/controller/user.controller.js
+++ Backend/controller/user.controller.js
@@ -1,5 +1,6 @@
 import User from "../models/user.model.js";
-import bcrypet from "bcryptjs";
+import bcrypt from "bcryptjs";
+import createTokenAndSaveCookie from "../jwt/generateToken.js";
 
 export const signup=async(req,res)=>{
     
@@ -16,7 +17,7 @@ export const signup=async(req,res)=>{
             return res.status(400).json({message:"User already exists"})
         }
         //Hashing the password
-        const hashPassword = await bcrypet.hash(password, 10);
+        const hashPassword = await bcrypt.hash(password, 10);
 
         // we will store this information in database
         const newUser = await new User({
@@ -26,11 +27,49 @@ export const signup=async(req,res)=>{
         })
         //saving in database
         await newUser.save();
-        res.status(201).json({message:"User created successfully"});
-
+        
+        //generating token
+        if(newUser){
+            createTokenAndSaveCookie(newUser._id,res)
+            res.status(201).json({message:"User created successfully",newUser});
+        }
     } 
     catch (error) {
         console.log(error);
         res.status(500).json({error:"Internal server error"})
+    }
+};
+
+export const login=async(req,res)=>{
+    const {email,password}= req.body;
+    try {
+        // here User is model which we created
+        const user = await User.findOne({email})
+        // user is the single document which we receive and we want the password from it so we do user.password
+        const isMatch = await bcrypt.compare(password,user.password)
+        if(!user || !isMatch){
+            return res.status(400).json({message:"Invalid User Credential"})
+        }
+        createTokenAndSaveCookie(user._id,res);
+        res.status(200).json({message:"User logged in successfully",user:{
+            _id: user._id,
+            fullname: user.fullname,
+            email: user.email
+
+        }});
+        
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({error:"Internal server error"});
+    }
+}
+
+export const logout = async (req,res)=>{
+    try {
+        res.clearCookie('jwt');
+        res.status(201).json({message:"User logged out successfully"});
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({error:"Internal server error"});
     }
 }
--- Backend/routes/user.route.js
+++ Backend/routes/user.route.js
@@ -1,9 +1,11 @@
 import express from "express";
-import { signup } from "../controller/user.controller.js";
+import { login, logout, signup } from "../controller/user.controller.js";
 // yaha pe signup {} main hai which means in user.controller there are more api and we are calling the specific exported api which is named as signup there
 
 const router = express.Router();
 
 router.post("/signup",signup);
+router.post("/login",login);
+router.post("/logout",logout);
 
 export default router;
